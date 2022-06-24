14b43f4e6d86624efee1febbb671835e1ab9efe3
Mario Sergio Machado dos Santos <29000264+msmachadodossantos@users.noreply.github.com>
2022-06-24 15:32:55 -0300

    Update init.bat

--- init.bat
+++ init.bat
@@ -6,7 +6,9 @@
 @REM ----------
 IF NOT EXIST ".gitignore" (REM. > ".gitignore")
 IF NOT EXIST "CHANGELOG.md" (REM. > "CHANGELOG.md")
+IF NOT EXIST "gulpfile.js" (REM. > "gulpfile.js")
 IF NOT EXIST "init.bat" (REM. > "init.bat")
 IF NOT EXIST "LICENSE" (REM. > "LICENSE")
 IF NOT EXIST "package.json" (REM. > "package.json")
 IF NOT EXIST "README.md" (REM. > "README.md")
+IF NOT EXIST "tsconfig.json" (REM. > "tsconfig.json")
