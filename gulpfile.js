6195881aa9eeab0c32536649916ac3a2ded7ede4
Mario Sergio Machado dos Santos <29000264+msmachadodossantos@users.noreply.github.com>
2022-06-25 09:02:05 -0300

    Update gulpfile.js
    Add: gulp-clean

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,26 @@
 const gulp = require("gulp");
 const {src, dest, series, parallel} = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const concat = require('gulp-concat');
 const uglify = require("gulp-uglify");
+const clean = require('gulp-clean');
+
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+const devSrc = "src/scss/**/*.scss";
+const devDest = "dist/css/dev/";
+
+function dev() {
+    return gulp.src(devSrc)
+        .pipe(sass({outputStyle: 'compressed'}, '').on("error", sass.logError))
+        .pipe(concat('dev.css'))
+        .pipe(dest(devDest));
+}
+
+function cleanDev() {
+    return gulp.src(devDest, {read: false})
+        .pipe(clean());
+}
+
+exports.dev = series(cleanDev, dev);
